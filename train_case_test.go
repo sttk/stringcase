3a7411fc6288aab0d6573070cbd58d2bb64aa987
sttk <sttk.xslet@gmail.com>
2025-02-08 16:37:42 +0900

    test: modified test cases of Train-Case

--- train_case_test.go
+++ train_case_test.go
@@ -829,7 +829,7 @@ func TestTrainCaseWithOptions(t *testing.T) {
 		})
 	})
 
-	t.Run("non-alphabets as head of a word and with separators", func(t *testing.T) {
+	t.Run("non-alphabets as head of a word and with kept characters", func(t *testing.T) {
 		origOpts := stringcase.Options{
 			SeparateBeforeNonAlphabets: true,
 			SeparateAfterNonAlphabets:  false,
@@ -943,7 +943,7 @@ func TestTrainCaseWithOptions(t *testing.T) {
 		})
 	})
 
-	t.Run("non-alphabets as tail of a word and with separators", func(t *testing.T) {
+	t.Run("non-alphabets as tail of a word and with kept characters", func(t *testing.T) {
 		origOpts := stringcase.Options{
 			SeparateBeforeNonAlphabets: false,
 			SeparateAfterNonAlphabets:  true,
@@ -1055,124 +1055,124 @@ func TestTrainCaseWithOptions(t *testing.T) {
 			result := stringcase.TrainCaseWithOptions("", opts)
 			assert.Equal(t, result, "")
 		})
+	})
 
-		t.Run("non-alphabets as a word and with separators", func(t *testing.T) {
-			origOpts := stringcase.Options{
-				SeparateBeforeNonAlphabets: true,
-				SeparateAfterNonAlphabets:  true,
-			}
+	t.Run("non-alphabets as a word and with kept characters", func(t *testing.T) {
+		origOpts := stringcase.Options{
+			SeparateBeforeNonAlphabets: true,
+			SeparateAfterNonAlphabets:  true,
+		}
 
-			t.Run("convert camelCase", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = "-_"
-				result := stringcase.TrainCaseWithOptions("abcDefGHIjk", opts)
-				assert.Equal(t, result, "Abc-Def-Gh-Ijk")
-			})
+		t.Run("convert camelCase", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = "-_"
+			result := stringcase.TrainCaseWithOptions("abcDefGHIjk", opts)
+			assert.Equal(t, result, "Abc-Def-Gh-Ijk")
+		})
 
-			t.Run("convert PascalCase", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = "-_"
-				result := stringcase.TrainCaseWithOptions("AbcDefGHIjk", opts)
-				assert.Equal(t, result, "Abc-Def-Gh-Ijk")
-			})
+		t.Run("convert PascalCase", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = "-_"
+			result := stringcase.TrainCaseWithOptions("AbcDefGHIjk", opts)
+			assert.Equal(t, result, "Abc-Def-Gh-Ijk")
+		})
 
-			t.Run("convert snake_case", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = "-"
-				result := stringcase.TrainCaseWithOptions("abc_def_ghi", opts)
-				assert.Equal(t, result, "Abc-Def-Ghi")
+		t.Run("convert snake_case", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = "-"
+			result := stringcase.TrainCaseWithOptions("abc_def_ghi", opts)
+			assert.Equal(t, result, "Abc-Def-Ghi")
 
-				opts.Keep = "_"
-				result = stringcase.TrainCaseWithOptions("abc_def_ghi", opts)
-				assert.Equal(t, result, "Abc-_-Def-_-Ghi")
-			})
+			opts.Keep = "_"
+			result = stringcase.TrainCaseWithOptions("abc_def_ghi", opts)
+			assert.Equal(t, result, "Abc-_-Def-_-Ghi")
+		})
 
-			t.Run("convert kebab-case", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = "_"
-				result := stringcase.TrainCaseWithOptions("abc-def-ghi", opts)
-				assert.Equal(t, result, "Abc-Def-Ghi")
+		t.Run("convert kebab-case", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = "_"
+			result := stringcase.TrainCaseWithOptions("abc-def-ghi", opts)
+			assert.Equal(t, result, "Abc-Def-Ghi")
 
-				opts.Keep = "-"
-				result = stringcase.TrainCaseWithOptions("abc-def-ghi", opts)
-				assert.Equal(t, result, "Abc---Def---Ghi")
-			})
+			opts.Keep = "-"
+			result = stringcase.TrainCaseWithOptions("abc-def-ghi", opts)
+			assert.Equal(t, result, "Abc---Def---Ghi")
+		})
 
-			t.Run("convert Train-Case", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = "_"
-				result := stringcase.TrainCaseWithOptions("Abc-Def-Ghi", opts)
-				assert.Equal(t, result, "Abc-Def-Ghi")
+		t.Run("convert Train-Case", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = "_"
+			result := stringcase.TrainCaseWithOptions("Abc-Def-Ghi", opts)
+			assert.Equal(t, result, "Abc-Def-Ghi")
 
-				opts.Keep = "-"
-				result = stringcase.TrainCaseWithOptions("Abc-Def-Ghi", opts)
-				assert.Equal(t, result, "Abc---Def---Ghi")
-			})
+			opts.Keep = "-"
+			result = stringcase.TrainCaseWithOptions("Abc-Def-Ghi", opts)
+			assert.Equal(t, result, "Abc---Def---Ghi")
+		})
 
-			t.Run("convert MACRO_CASE", func(t *testing.T) {
-				opts := origOpts
+		t.Run("convert MACRO_CASE", func(t *testing.T) {
+			opts := origOpts
 
-				opts.Keep = "-"
-				result := stringcase.TrainCaseWithOptions("ABC_DEF_GHI", opts)
-				assert.Equal(t, result, "Abc-Def-Ghi")
+			opts.Keep = "-"
+			result := stringcase.TrainCaseWithOptions("ABC_DEF_GHI", opts)
+			assert.Equal(t, result, "Abc-Def-Ghi")
 
-				opts.Keep = "_"
-				result = stringcase.TrainCaseWithOptions("ABC_DEF_GHI", opts)
-				assert.Equal(t, result, "Abc-_-Def-_-Ghi")
-			})
+			opts.Keep = "_"
+			result = stringcase.TrainCaseWithOptions("ABC_DEF_GHI", opts)
+			assert.Equal(t, result, "Abc-_-Def-_-Ghi")
+		})
 
-			t.Run("convert COBOL-CASE", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = "_"
-				result := stringcase.TrainCaseWithOptions("ABC-DEF-GHI", opts)
-				assert.Equal(t, result, "Abc-Def-Ghi")
+		t.Run("convert COBOL-CASE", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = "_"
+			result := stringcase.TrainCaseWithOptions("ABC-DEF-GHI", opts)
+			assert.Equal(t, result, "Abc-Def-Ghi")
 
-				opts.Keep = "-"
-				result = stringcase.TrainCaseWithOptions("ABC-DEF-GHI", opts)
-				assert.Equal(t, result, "Abc---Def---Ghi")
-			})
+			opts.Keep = "-"
+			result = stringcase.TrainCaseWithOptions("ABC-DEF-GHI", opts)
+			assert.Equal(t, result, "Abc---Def---Ghi")
+		})
 
-			t.Run("convert with keeping digits", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = "_"
-				result := stringcase.TrainCaseWithOptions("abc123-456defG89HIJklMN12", opts)
-				assert.Equal(t, result, "Abc-123-456-Def-G-89-Hi-Jkl-Mn-12")
+		t.Run("convert with keeping digits", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = "_"
+			result := stringcase.TrainCaseWithOptions("abc123-456defG89HIJklMN12", opts)
+			assert.Equal(t, result, "Abc-123-456-Def-G-89-Hi-Jkl-Mn-12")
 
-				opts.Keep = "-"
-				result = stringcase.TrainCaseWithOptions("abc123-456defG89HIJklMN12", opts)
-				assert.Equal(t, result, "Abc-123-456-Def-G-89-Hi-Jkl-Mn-12")
-			})
+			opts.Keep = "-"
+			result = stringcase.TrainCaseWithOptions("abc123-456defG89HIJklMN12", opts)
+			assert.Equal(t, result, "Abc-123-456-Def-G-89-Hi-Jkl-Mn-12")
+		})
 
-			t.Run("convert with symbols as separators", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = ".~!#%?"
-				result := stringcase.TrainCaseWithOptions(":.abc~!@def#$ghi%&jk(lm)no/?", opts)
-				assert.Equal(t, result, ".-Abc-~!-Def-#-Ghi-%-Jk-Lm-No-?")
-			})
+		t.Run("convert with symbols as separators", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = ".~!#%?"
+			result := stringcase.TrainCaseWithOptions(":.abc~!@def#$ghi%&jk(lm)no/?", opts)
+			assert.Equal(t, result, ".-Abc-~!-Def-#-Ghi-%-Jk-Lm-No-?")
+		})
 
-			t.Run("convert when starting with digit", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = "-_"
-				result := stringcase.TrainCaseWithOptions("123abc456def", opts)
-				assert.Equal(t, result, "123-Abc-456-Def")
+		t.Run("convert when starting with digit", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = "-_"
+			result := stringcase.TrainCaseWithOptions("123abc456def", opts)
+			assert.Equal(t, result, "123-Abc-456-Def")
 
-				result = stringcase.TrainCaseWithOptions("123ABC456DEF", opts)
-				assert.Equal(t, result, "123-Abc-456-Def")
+			result = stringcase.TrainCaseWithOptions("123ABC456DEF", opts)
+			assert.Equal(t, result, "123-Abc-456-Def")
 
-				result = stringcase.TrainCaseWithOptions("123Abc456Def", opts)
-				assert.Equal(t, result, "123-Abc-456-Def")
-			})
+			result = stringcase.TrainCaseWithOptions("123Abc456Def", opts)
+			assert.Equal(t, result, "123-Abc-456-Def")
+		})
 
-			t.Run("convert an empty string", func(t *testing.T) {
-				opts := origOpts
-				opts.Keep = "-_"
-				result := stringcase.TrainCaseWithOptions("", opts)
-				assert.Equal(t, result, "")
-			})
+		t.Run("convert an empty string", func(t *testing.T) {
+			opts := origOpts
+			opts.Keep = "-_"
+			result := stringcase.TrainCaseWithOptions("", opts)
+			assert.Equal(t, result, "")
 		})
 	})
 
-	t.Run("non-alphabets as part of a word and with separators", func(t *testing.T) {
+	t.Run("non-alphabets as part of a word and with kept characters", func(t *testing.T) {
 		origOpts := stringcase.Options{
 			SeparateBeforeNonAlphabets: false,
 			SeparateAfterNonAlphabets:  false,
