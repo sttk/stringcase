459a29f7e2bc34d18d9dbffff77f004e73d2355a
Takayuki Sato <sttk.xslet@gmail.com>
2026-08-26 23:55:24 +0900

    chore: removed errcheck and used set -euo pipefail instead in build.sh (#50)

--- build.sh
+++ build.sh
@@ -1,46 +1,32 @@
 #!/usr/bin/env bash
 
-errcheck() {
-  exitcd=$1
-  if [[ "$exitcd" != "0" ]]; then
-    exit $exitcd
-  fi
-}
+set -euo pipefail
 
 clean() {
   go clean --cache
-  errcheck $?
 }
 
 format() {
   go fmt ./...
-  errcheck $?
 }
 
 compile() {
   go vet ./...
-  errcheck $?
   go build ./...
-  errcheck $?
 }
 
 test() {
   go test -v ./...
-  errcheck $?
 }
 
 unit() {
   go test -v ./... -run $1
-  errcheck $?
 }
 
 cover() {
   mkdir -p coverage
-  errcheck $?
   go test -coverprofile=coverage/cover.out ./...
-  errcheck $?
   go tool cover -html=coverage/cover.out -o coverage/cover.html
-  errcheck $?
 }
 
 bench() {
@@ -50,7 +36,6 @@ bench() {
   fi
   pushd $dir
   go test -bench . --benchmem -run=^$
-  errcheck $?
   popd
 }
 
